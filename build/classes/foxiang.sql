7952672f3063ef0876b5e1bd0ad16d5790580c80
liwenlong <1798946643@qq.com>
2019-05-24 12:14:24 +0800

    foxiang.sql

---
+++ build/classes/foxiang.sql
@@ -0,0 +1,22 @@
+﻿# Host: localhost  (Version 8.0.15)
+# Date: 2019-03-20 19:46:27
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "user"
+#
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+#
+# Data for table "user"
+#
+
+INSERT INTO `user` VALUES (1,'root','lwl.1993');
